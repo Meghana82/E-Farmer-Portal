19ff498a180672f577ec30c28d36e06a92d25233
Meghana82 <63181485+Meghana82@users.noreply.github.com>
2021-06-04 20:47:00 +0530

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,9 @@
+function opennav(){
+  document.getElementById("nav-menu").style.width="300px";
+  document.getElementById("menuicon").style.width="none";
+}
+
+function closnav(){
+  document.getElementById("nav-menu").style.width="0";
+  document.getElementById("menuicon").style.width="block";
+}
